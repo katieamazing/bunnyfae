29e57c5132e7a33fcf9ce759fc09a02a8605ef63
Glitch (bunnyfae) <none>
2018-06-14 22:36:52 +0000

    🐠⛑ Checkpoint ./.glitch-assets:677510/2298 ./index.html:677510/13178 ./script.js:677510/1131

--- script.js
+++ script.js
@@ -3,3 +3,33 @@
 
 // prints "hi" in the browser's dev tools console
 console.log('hi');
+
+var canvas = document.getElementById("thecanvas");
+var ctx = canvas.getContext("2d", { alpha: false });
+const SCREEN_WIDTH = canvas.width;
+const SCREEN_HEIGHT = canvas.height;
+
+function draw(absoluteMs, ctx) {
+  ctx.clearRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+  ctx.beginPath();
+  ctx.rect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+  ctx.fillStyle = "gray";
+  ctx.fill();
+}
+
+var previousFrameMs = null;
+function tick(absoluteMs) {
+  if (previousFrameMs === null) {
+    previousFrameMs = absoluteMs;
+  }
+  var deltaMs = absoluteMs - previousFrameMs;
+  // console.log(deltaMs);
+  if (deltaMs > 100) {
+    deltaMs = 100;
+  }
+  previousFrameMs = absoluteMs;
+  update(deltaMs);
+  draw(absoluteMs, ctx);
+  requestAnimationFrame(tick);
+}
+requestAnimationFrame(tick);
